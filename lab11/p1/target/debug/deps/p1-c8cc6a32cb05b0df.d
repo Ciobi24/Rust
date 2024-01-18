61976a02d8d057f826c315a8db7d2d3b441134b8
Andra Ciobanu <119164551+Ciobi24@users.noreply.github.com>
2024-01-18 11:11:31 +0200

    Delete lab11/p1/target directory

--- lab11/p1/target/debug/deps/p1-c8cc6a32cb05b0df.d
+++
@@ -1,5 +0,0 @@
-C:\Facultate\rust\git\Rust\lab11\p1\target\debug\deps\p1-c8cc6a32cb05b0df.rmeta: src\main.rs
-
-C:\Facultate\rust\git\Rust\lab11\p1\target\debug\deps\p1-c8cc6a32cb05b0df.d: src\main.rs
-
-src\main.rs:
--- lab11/p1/target/debug/p1.d
+++
@@ -1,1 +0,0 @@
-C:\Facultate\rust\git\Rust\lab11\p1\target\debug\p1.exe: C:\Facultate\rust\git\Rust\lab11\p1\src\main.rs
